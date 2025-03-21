e26a8779dac015e8126e624ad5f8fec1adaeb8f0
Chris Trewin <chris.trewin@gmail.com>
2025-03-21 16:02:28 +1100

    removed greet stuff added messages stuff

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-export const greet = (name: string): string => {
-  return `Hello, ${name}, from greet @ Semantic Release Demo!`;
-};
-
-export const greets = (name: string): string => {
-  return `Hello, ${name}, from greets @ Semantic Release Demo!`;
-};
-
-export const greeters = (name: string): string => {
-  return `Hello, ${name}, from greeters @ Semantic Release Demo!`;
+export const messages = (name: string): string => {
+  return `Hello, ${name}, from messages @ Semantic Release Demo!`;
 };
