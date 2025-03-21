101050ca430ca1fcf2b6097e078a8becaf45df62
Chris Trewin <chris.trewin@gmail.com>
2025-03-21 11:23:35 +1100

    feat(greeter) added greeter and brought back greet

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,7 @@ export const greet = (name: string): string => {
 export const greets = (name: string): string => {
   return `Hello, ${name}, from greets @ Semantic Release Demo!`;
 };
+
+export const greeter = (name: string): string => {
+  return `Hello, ${name}, from greeter @ Semantic Release Demo!`;
+};
