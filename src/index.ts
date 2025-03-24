ade4f6c388e11a22539fd38c47d152c8ad8fe5de
Chris Trewin <chris.trewin@gmail.com>
2025-03-25 07:59:21 +1100

    feat: added world fn

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export const hello = (name: string): string => {
   return `Hello, ${name}, from Semantic Release Demo!`;
 };
+
+export const world = (name: string): string => {
+  return `Hello, ${name}, from Semantic Release Demo!`;
+};
